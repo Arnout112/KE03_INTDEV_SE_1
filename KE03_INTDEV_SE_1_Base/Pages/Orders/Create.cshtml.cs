561d253c0c47490a1df55afec088e418cb8bc18d
Arnout112 <arnout.r.t.smit@gmail.com>
2025-06-17 10:32:20 +0200

    Create order vieuws

---
+++ KE03_INTDEV_SE_1_Base/Pages/Orders/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using DataAccessLayer;
+using DataAccessLayer.Models;
+
+namespace KE03_INTDEV_SE_1_Base.Pages.Orders
+{
+    public class CreateModel : PageModel
+    {
+        private readonly DataAccessLayer.MatrixIncDbContext _context;
+
+        public CreateModel(DataAccessLayer.MatrixIncDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Address");
+            return Page();
+        }
+
+        [BindProperty]
+        public Order Order { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Orders.Add(Order);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ KE03_INTDEV_SE_1_Base/Pages/Orders/Edit.cshtml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DataAccessLayer;
+using DataAccessLayer.Models;
+
+namespace KE03_INTDEV_SE_1_Base.Pages.Orders
+{
+    public class EditModel : PageModel
+    {
+        private readonly DataAccessLayer.MatrixIncDbContext _context;
+
+        public EditModel(DataAccessLayer.MatrixIncDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Order Order { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order =  await _context.Orders.FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            Order = order;
+           ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Address");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Order).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(Order.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Orders.Any(e => e.Id == id);
+        }
+    }
+}
